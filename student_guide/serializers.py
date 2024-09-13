3137aa1f08130b53249623b59d8197526d4c88dc
amm-ar <amar9683@outlook.com>
2024-09-13 19:08:08 +0200

    Version 1.0

---
+++ student_guide/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Chapter, GuideTopic
+
+class ChapterSerializer(ModelSerializer):
+    class Meta:
+        model = Chapter
+        fields = '__all__'
+        
+
+class GuideTopicSerializer(ModelSerializer):
+    class Meta:
+        model = GuideTopic
+        fields = '__all__'#['id', 'title', 'ar_title', 'chapter', 'created', 'updated', 'content', 'ar_content', 'image']
+        
